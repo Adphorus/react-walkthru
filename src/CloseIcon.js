6240b21815a65091c4f2ed12db9090d979d0ca74
Burak Can <neoberg@gmail.com>
2015-11-26 15:27:39 +0200

    unuse stateles function component because its not compatible with react 0.13.x

--- src/CloseIcon.js
+++ src/CloseIcon.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function(props) {
-  return (
-    <svg viewBox='0 0 24 24' preserveAspectRatio='xMidYMid meet' fit style={ props.style } onClick={ props.onClick }>
-      <g><path d='M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z'></path></g>
-    </svg>
-  )
+export default class CloseIcon extends Component {
+  render() {
+    return (
+      <svg viewBox='0 0 24 24' preserveAspectRatio='xMidYMid meet' fit style={ this.props.style } onClick={ this.props.onClick }>
+        <g><path d='M19 6.41l-1.41-1.41-5.59 5.59-5.59-5.59-1.41 1.41 5.59 5.59-5.59 5.59 1.41 1.41 5.59-5.59 5.59 5.59 1.41-1.41-5.59-5.59z'></path></g>
+      </svg>
+    )
+  }
 }
